2be6ec59cb8db40ec00509ae64ad407a80370e3f
DevExpressExampleBot <fakeEmail>
2021-09-06 22:48:17 +0300

    autogenerate VB

--- VB/WpfApplication25/MainWindow.xaml.vb
+++ VB/WpfApplication25/MainWindow.xaml.vb
@@ -16,42 +16,42 @@ Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Grid
 
 Namespace WpfApplication25
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-        Private Sub MyScrollBarCustomRowAnnotationEventHandler(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.ScrollBarCustomRowAnnotationEventArgs)
-            Dim data As TestData = TryCast(e.Row, TestData)
-            If data Is Nothing Then
-                Return
-            End If
-            Dim number As Integer = data.Number
-            If number > 10 AndAlso number < 15 Then
-                ShowCustomScrollAnnotation(e, Brushes.LightCoral)
-            End If
-            If number > 2 AndAlso number < 4 Then
-                ShowCustomScrollAnnotation(e, Brushes.Green)
-            End If
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub MyScrollBarCustomRowAnnotationEventHandler(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.ScrollBarCustomRowAnnotationEventArgs)
+			Dim data As TestData = TryCast(e.Row, TestData)
+			If data Is Nothing Then
+				Return
+			End If
+			Dim number As Integer = data.Number
+			If number > 10 AndAlso number < 15 Then
+				ShowCustomScrollAnnotation(e, Brushes.LightCoral)
+			End If
+			If number > 2 AndAlso number < 4 Then
+				ShowCustomScrollAnnotation(e, Brushes.Green)
+			End If
 
-        End Sub
-        Private Sub ShowCustomScrollAnnotation(ByVal e As DevExpress.Xpf.Grid.ScrollBarCustomRowAnnotationEventArgs, ByVal brush As SolidColorBrush)
-            e.ScrollBarAnnotationInfo = New DevExpress.Xpf.Grid.ScrollBarAnnotationInfo() With { _
-                .Alignment = DevExpress.Xpf.Grid.ScrollBarAnnotationAlignment.Right, _
-                .Brush = brush, _
-                .MinHeight = 3, _
-                .Width = 10 _
-            }
-        End Sub
+		End Sub
+		Private Sub ShowCustomScrollAnnotation(ByVal e As DevExpress.Xpf.Grid.ScrollBarCustomRowAnnotationEventArgs, ByVal brush As SolidColorBrush)
+			e.ScrollBarAnnotationInfo = New DevExpress.Xpf.Grid.ScrollBarAnnotationInfo() With {
+				.Alignment = DevExpress.Xpf.Grid.ScrollBarAnnotationAlignment.Right,
+				.Brush = brush,
+				.MinHeight = 3,
+				.Width = 10
+			}
+		End Sub
 
-        Private Sub MyLoadedEventHandler(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim view As TableView = TryCast(sender, TableView)
-            view.SearchString = "Element2"
-            view.SelectCells(15, view.Grid.Columns(0), 25, view.Grid.Columns(1))
-        End Sub
-    End Class
+		Private Sub MyLoadedEventHandler(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim view As TableView = TryCast(sender, TableView)
+			view.SearchString = "Element2"
+			view.SelectCells(15, view.Grid.Columns(0), 25, view.Grid.Columns(1))
+		End Sub
+	End Class
 End Namespace
--- VB/WpfApplication25/ViewModel.vb
+++ VB/WpfApplication25/ViewModel.vb
@@ -8,89 +8,89 @@ Imports System.Text
 Imports System.Threading.Tasks
 
 Namespace WpfApplication25
-    Public Class ViewModel
-        Public Property List() As ObservableCollection(Of TestData)
+	Public Class ViewModel
+		Public Property List() As ObservableCollection(Of TestData)
 
-        Public Sub New()
-            List = New ObservableCollection(Of TestData)()
-            PopulateData()
-        End Sub
+		Public Sub New()
+			List = New ObservableCollection(Of TestData)()
+			PopulateData()
+		End Sub
 
-        Private Sub PopulateData()
-            For i As Integer = 0 To 29
-                Dim t = New TestData(i, "Element" & i.ToString())
-                List.Add(t)
-            Next i
-        End Sub
-    End Class
+		Private Sub PopulateData()
+			For i As Integer = 0 To 29
+				Dim t = New TestData(i, "Element" & i.ToString())
+				List.Add(t)
+			Next i
+		End Sub
+	End Class
 
-    Public Class TestData
-        Implements INotifyPropertyChanged, IDataErrorInfo
+	Public Class TestData
+		Implements INotifyPropertyChanged, IDataErrorInfo
 
+'INSTANT VB NOTE: The field number was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private number_Conflict As Integer
+'INSTANT VB NOTE: The field text was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private text_Conflict As String
 
-        Private number_Renamed As Integer
+		Public Sub New(ByVal number As Integer, ByVal text As String)
+			Me.number_Conflict = number
+			Me.text_Conflict = text
+		End Sub
 
-        Private text_Renamed As String
+		Public Property Number() As Integer
+			Get
+				Return number_Conflict
+			End Get
+			Set(ByVal value As Integer)
+				If number_Conflict = value Then
+					Return
+				End If
+				number_Conflict = value
+				NotifyChanged("Number")
+			End Set
+		End Property
+		Public Property Text() As String
+			Get
+				Return text_Conflict
+			End Get
+			Set(ByVal value As String)
+				If text_Conflict = value Then
+					Return
+				End If
+				text_Conflict = value
+				NotifyChanged("Text")
+			End Set
+		End Property
 
-        Public Sub New(ByVal number As Integer, ByVal text As String)
-            Me.number_Renamed = number
-            Me.text_Renamed = text
-        End Sub
+		Public ReadOnly Property [Error]() As String
+			Get
+				Dim result As String = String.Empty
+				If Number >= 10 AndAlso Number < 20 Then
+					result = "Incorrect value!"
+				End If
+				Return result
+			End Get
+		End Property
 
-        Public Property Number() As Integer
-            Get
-                Return number_Renamed
-            End Get
-            Set(ByVal value As Integer)
-                If number_Renamed = value Then
-                    Return
-                End If
-                number_Renamed = value
-                NotifyChanged("Number")
-            End Set
-        End Property
-        Public Property Text() As String
-            Get
-                Return text_Renamed
-            End Get
-            Set(ByVal value As String)
-                If text_Renamed = value Then
-                    Return
-                End If
-                text_Renamed = value
-                NotifyChanged("Text")
-            End Set
-        End Property
+		Default Public ReadOnly Property Item(ByVal columnName As String) As String
+			Get
+				Dim result As String = String.Empty
+				Select Case columnName
+					Case "Number"
+						If Number > 20 AndAlso Number <= 40 Then
+							result = String.Format("The current number is less than {0} !", Number.ToString())
+						End If
+				End Select
+				Return result
+			End Get
+		End Property
 
-        Public ReadOnly Property [Error]() As String Implements IDataErrorInfo.Error
-            Get
-                Dim result As String = String.Empty
-                If Number >= 10 AndAlso Number < 20 Then
-                    result = "Incorrect value!"
-                End If
-                Return result
-            End Get
-        End Property
+		Private Sub NotifyChanged(ByVal propertyName As String)
+			RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+		End Sub
 
-        Default Public ReadOnly Property Item(ByVal columnName As String) As String Implements IDataErrorInfo.Item
-            Get
-                Dim result As String = String.Empty
-                Select Case columnName
-                    Case "Number"
-                        If Number > 20 AndAlso Number <= 40 Then
-                            result = String.Format("The current number is less than {0} !", Number.ToString())
-                        End If
-                End Select
-                Return result
-            End Get
-        End Property
-
-        Private Sub NotifyChanged(ByVal propertyName As String)
-            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
-        End Sub
-
-        #Region "INotifyPropertyChanged Members"
-        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
-        #End Region
-    End Class
+		#Region "INotifyPropertyChanged Members"
+		Public Event PropertyChanged As PropertyChangedEventHandler
+		#End Region
+	End Class
 End Namespace
